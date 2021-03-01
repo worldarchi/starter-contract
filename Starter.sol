9ab89083af5d7f4b7b0bc03e22767ffd03a74633
yiepier <garmoniart@gmail.com>
2021-03-01 18:20:16 +0800

    +Offering

--- Starter.sol
+++ Starter.sol
@@ -98,3 +98,138 @@ contract Starter is Configurable {
     }
     event Withdrawn(address to, uint amount, uint volume);
 }
+
+contract Offering is Configurable {
+	using SafeMath for uint;
+	using SafeERC20 for IERC20;
+	
+	IERC20 public currency;
+	IERC20 public token;
+	uint public ratio;
+	address payable public recipient;
+	uint public timeOffer;
+	uint public timeClaim;
+	
+	uint public totalQuota;
+	uint public totalOffered;
+	uint public totalClaimed;
+	mapping (address => uint) public quotaOf;
+	mapping (address => uint) public offeredOf;
+	mapping (address => uint) public claimedOf;
+
+	function __Offering_init(address governor_, address currency_, address token_, uint ratio_, address payable recipient_, uint timeOffer_, uint timeClaim_) external initializer {
+		__Governable_init_unchained(governor_);
+		__Offering_init_unchained(currency_, token_, ratio_, recipient_, timeOffer_, timeClaim_);
+	}
+	
+	function __Offering_init_unchained(address currency_, address token_, uint ratio_, address payable recipient_, uint timeOffer_, uint timeClaim_) public governance {
+		currency = IERC20(currency_);
+		token = IERC20(token_);
+		ratio = ratio_;
+		recipient = recipient_;
+	    timeOffer = timeOffer_;
+	    timeClaim = timeClaim_;
+	}
+	
+    function setQuota(address addr, uint amount) public governance {
+        totalQuota = totalQuota.add(amount).sub(quotaOf[addr]);
+        quotaOf[addr] = amount;
+        emit Quota(addr, amount, totalQuota);
+    }
+    event Quota(address indexed addr, uint amount, uint total);
+    
+    function setQuotas(address[] memory addrs, uint amount) external {
+        for(uint i=0; i<addrs.length; i++)
+            setQuota(addrs[i], amount);
+    }
+    
+    function setQuotas(address[] memory addrs, uint[] memory amounts) external {
+        for(uint i=0; i<addrs.length; i++)
+            setQuota(addrs[i], amounts[i]);
+    }
+    
+	function offer(uint amount) external {
+		require(address(currency) != address(0), 'should call offerHT() instead');
+		require(now >= timeOffer, "it's not time yet");
+		require(now < timeClaim, "expired");
+		amount = Math.min(amount, quotaOf[msg.sender]);
+		require(amount > 0, 'no quota');
+		require(currency.allowance(msg.sender, address(this)) >= amount, 'allowance not enough');
+		require(currency.balanceOf(msg.sender) >= amount, 'balance not enough');
+		require(offeredOf[msg.sender] == 0, 'offered already');
+		
+		currency.safeTransferFrom(msg.sender, recipient, amount);
+		uint volume = amount.mul(ratio).div(1e18);
+		offeredOf[msg.sender] = volume;
+		totalOffered = totalOffered.add(volume);
+		require(totalOffered <= token.balanceOf(address(this)), 'Quota is full');
+		emit Offer(msg.sender, amount, volume, totalOffered);
+	}
+	event Offer(address indexed addr, uint amount, uint volume, uint total);
+	
+	function offerHT() public payable {
+		require(address(currency) == address(0), 'should call offer(uint amount) instead');
+		require(now >= timeOffer, "it's not time yet");
+		require(now < timeClaim, "expired");
+		uint amount = Math.min(msg.value, quotaOf[msg.sender]);
+		require(amount > 0, 'no quota');
+		require(offeredOf[msg.sender] == 0, 'offered already');
+		
+		recipient.transfer(amount);
+		uint volume = amount.mul(ratio).div(1e18);
+		offeredOf[msg.sender] = volume;
+		totalOffered = totalOffered.add(volume);
+		require(totalOffered <= token.balanceOf(address(this)), 'Quota is full');
+		if(msg.value > amount)
+		    msg.sender.transfer(msg.value.sub(amount));
+		emit Offer(msg.sender, amount, volume, totalOffered);
+	}
+
+    function claim() public {
+        require(now >= timeClaim, "it's not time yet");
+        require(claimedOf[msg.sender] == 0, 'claimed already'); 
+		if(token.balanceOf(address(this)).add(totalClaimed) > totalOffered)
+			token.safeTransfer(recipient, token.balanceOf(address(this)).add(totalClaimed).sub(totalOffered));
+        uint volume = offeredOf[msg.sender];
+        claimedOf[msg.sender] = volume;
+        totalClaimed = totalClaimed.add(volume);
+        token.safeTransfer(msg.sender, volume);
+        emit Claim(msg.sender, volume, totalClaimed);
+    }
+    event Claim(address indexed addr, uint volume, uint total);
+    
+    /// @notice This method can be used by the owner to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    function rescueTokens(address _token, address _dst) public governance {
+        uint balance = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).safeTransfer(_dst, balance);
+    }
+    
+    function withdrawToken(address _dst) external governance {
+        rescueTokens(address(token), _dst);
+    }
+
+    function withdrawToken() external governance {
+        rescueTokens(address(token), msg.sender);
+    }
+    
+    function withdrawHT(address payable _dst) external governance {
+        _dst.transfer(address(this).balance);
+    }
+    
+    function withdrawHT() external governance {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    receive() external payable{
+        if(msg.value > 0)
+            offerHT();
+        else
+            claim();
+    }
+    
+    fallback() external {
+        claim();
+    }
+}
